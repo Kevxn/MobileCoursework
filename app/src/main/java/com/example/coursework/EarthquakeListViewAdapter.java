a12fea632473ceff53c83c6da58592050cd7fb84
Kevin <kevingray1998@live.com>
2019-03-06 02:05:15 +0000

    Fixed formatting on recent page, added icons and colour

--- app/src/main/java/com/example/coursework/EarthquakeListViewAdapter.java
+++ app/src/main/java/com/example/coursework/EarthquakeListViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.coursework;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,12 +42,56 @@ public class EarthquakeListViewAdapter extends BaseAdapter {
         }
 
         QuakeItem q = (QuakeItem)getItem(position);
-        TextView txtLocation = (TextView)convertView.findViewById(R.id.lv_location);
-        TextView txtMagnitude = (TextView)convertView.findViewById(R.id.lv_magnitiude);
+        TextView txtLocation = (TextView)convertView.findViewById(R.id.txt_recent_Location);
+        TextView txtMagnitude = (TextView)convertView.findViewById(R.id.txt_recent_magnitude);
+        TextView txtDate = (TextView)convertView.findViewById(R.id.txt_recent_date);
+        TextView txtLatLon = (TextView)convertView.findViewById(R.id.txt_recent_LatLon);
 
         txtLocation.setText(q.getLocation());
         txtMagnitude.setText(Float.toString(q.getMagnitude()));
+        txtDate.setText(q.getDate().toString());
+        txtLatLon.setText(q.getLat() + ", " + q.getLon());
+
+        float mag = q.getMagnitude();
+        setQuakeColour(txtMagnitude, mag);
+
 
         return convertView;
+    }
+
+    public void setQuakeColour(TextView txtMagnitude, float mag){
+        if (mag < -0.7){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.light_steel_blue));
+        }
+        else if (mag >= -0.7 && mag < -0.3){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.powder_blue));
+        }
+        else if (mag >= -0.3 && mag < 0.2){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.aqua));
+        }
+        else if (mag >= 0.2 && mag < 0.5){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.light_sea_green));
+        }
+        else if (mag >= 0.5 && mag < 0.8){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.medium_sea_green));
+        }
+        else if (mag >= 0.8 && mag < 1.1){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.khaki));
+        }
+        else if (mag >= 1.1 && mag < 1.4){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.yellow));
+        }
+        else if (mag >= 1.4 && mag < 1.8){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.dark_orange));
+        }
+        else if (mag >= 1.8 && mag < 2.2){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.orange_red));
+        }
+        else if (mag >= 2.2 && mag < 2.5){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.red));
+        }
+        else if (mag >= 2.5 && mag > 2.8){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(mContext, R.color.dark_red));
+        }
     }
 }
