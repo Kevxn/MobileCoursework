701fb4917f5f575d5640b02da8f8a0b88a34e690
Kevin <kevingray1998@live.com>
2019-03-28 19:46:53 +0000

    Added map page with all quake markers

--- app/src/main/java/com/example/coursework/MapFragment.java
+++ app/src/main/java/com/example/coursework/MapFragment.java
@@ -1,16 +1,34 @@
 package com.example.coursework;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
+
+    MapView map;
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Nullable
     @Override
@@ -36,5 +54,135 @@ public class MapFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null){
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
+        }
+
+        final DataInterface data = new DataInterface();
+        data.startProgress();
+
+        map = view.findViewById(R.id.mapFragmentMapView);
+        map.onCreate(mapViewBundle);
+
+
+        new Thread(new Runnable() {
+
+            ArrayList<QuakeItem> items;
+            public void run() {
+                items=data.getQuakeItems();
+                try{
+                    if (items == null){
+                        // show loading
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+//                                progressBar.setVisibility(View.VISIBLE);
+//                                header.setVisibility(View.GONE);
+//                                itemList.setVisibility(View.GONE);
+                            }
+                        });
+                    }
+                    else{
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+//                                progressBar.setVisibility(View.GONE);
+//                                header.setVisibility(View.VISIBLE);
+//                                itemList.setVisibility(View.VISIBLE);
+                                for (QuakeItem item: items) {
+                                    float lat = item.getLat();
+                                    float lon = item.getLon();
+
+                                    final MarkerOptions marker = new MarkerOptions();
+                                    final LatLng point = new LatLng(lat, lon);
+                                    marker.position(point);
+
+                                    map.getMapAsync(new OnMapReadyCallback() {
+                                        @Override
+                                        public void onMapReady(GoogleMap googleMap) {
+
+                                            googleMap.addMarker(marker);
+                                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(point, 7f));
+                                        }
+                                    });
+                                }
+                            }
+                        });
+                    }
+                }
+                catch(Exception ex){
+                    Log.e("THREAD BROKEN: ", ex.getMessage());
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        map.onSaveInstanceState(mapViewBundle);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        map.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        map.onStop();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onPause() {
+        map.onPause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        map.onDestroy();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        map.onLowMemory();
     }
 }
