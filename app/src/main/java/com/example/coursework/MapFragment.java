8e8c04683250a00ed3f13a4dfa84b7cfde6a4952
Kevin <kevingray1998@live.com>
2019-03-28 23:12:12 +0000

    Implemented map fragment with functionality, bug fixes

--- app/src/main/java/com/example/coursework/MapFragment.java
+++ app/src/main/java/com/example/coursework/MapFragment.java
@@ -1,6 +1,7 @@
 package com.example.coursework;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Point;
 import android.os.Bundle;
@@ -8,20 +9,27 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.gson.Gson;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 
@@ -91,20 +99,74 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
 //                                progressBar.setVisibility(View.GONE);
 //                                header.setVisibility(View.VISIBLE);
 //                                itemList.setVisibility(View.VISIBLE);
-                                for (QuakeItem item: items) {
-                                    float lat = item.getLat();
+                                for (final QuakeItem item: items) {
+                                    final float lat = item.getLat();
                                     float lon = item.getLon();
 
-                                    final MarkerOptions marker = new MarkerOptions();
                                     final LatLng point = new LatLng(lat, lon);
-                                    marker.position(point);
+                                    //MarkerOptions markerOptions = new MarkerOptions();
+
 
                                     map.getMapAsync(new OnMapReadyCallback() {
                                         @Override
-                                        public void onMapReady(GoogleMap googleMap) {
+                                        public void onMapReady(final GoogleMap googleMap) {
 
-                                            googleMap.addMarker(marker);
-                                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(point, 7f));
+                                            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                                                    != PackageManager.PERMISSION_GRANTED
+                                                    && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                                                    != PackageManager.PERMISSION_GRANTED) {
+                                                return;
+                                            }
+
+                                            if (map != null){
+                                                googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                                                    @Override
+                                                    public View getInfoWindow(Marker marker) {
+                                                        return null;
+                                                    }
+
+                                                    @Override
+                                                    public View getInfoContents(Marker marker) {
+
+                                                        final QuakeItem quake = (QuakeItem) marker.getTag();
+
+                                                        View view = getLayoutInflater().inflate(R.layout.maps_popup, null);
+                                                        TextView location = view.findViewById(R.id.txt_map_Location);
+                                                        TextView datetime = view.findViewById(R.id.txt_map_date);
+                                                        TextView latlon = view.findViewById(R.id.txt_map_LatLon);
+                                                        TextView magnitude = view.findViewById(R.id.txt_map_magnitude);
+                                                        // Button btnViewEarthquake = view.findViewById(R.id.btn_map_view_earthquake);
+
+                                                        setQuakeColour(magnitude, quake.getMagnitude());
+                                                        DateFormat formattedDate = new SimpleDateFormat("dd/MM/yyyy");
+
+
+
+                                                        location.setText(quake.getLocation());
+                                                        datetime.setText(formattedDate.format(quake.getDate()));
+                                                        latlon.setText("" + Float.toString(quake.getLat()) + "°, " + Float.toString(quake.getLon()) + "°");
+                                                        magnitude.setText(Float.toString(quake.getMagnitude()));
+
+                                                        googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+                                                            @Override
+                                                            public void onInfoWindowClick(Marker marker) {
+                                                                Intent i = new Intent(getActivity(), DetailedQuakeViewActivity.class);
+                                                                i.putExtra("QuakeObject", new Gson().toJson(quake));
+                                                                getContext().startActivity(i);
+                                                            }
+                                                        });
+
+                                                        return view;
+                                                    }
+                                                });
+                                            }
+
+                                            googleMap.setMyLocationEnabled(true);
+
+                                            Marker marker = googleMap.addMarker(new MarkerOptions().position(point));
+                                            marker.setTag(item);
+
+                                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(point, 6f));
                                         }
                                     });
                                 }
@@ -133,6 +195,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     }
 
     @Override
+    public void onMapReady(GoogleMap map){
+
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
         map.onResume();
@@ -151,24 +218,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     }
 
     @Override
-    public void onMapReady(GoogleMap map) {
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        map.setMyLocationEnabled(true);
-    }
-
-    @Override
     public void onPause() {
         map.onPause();
         super.onPause();
@@ -184,5 +233,41 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     public void onLowMemory() {
         super.onLowMemory();
         map.onLowMemory();
+    }
+
+    public void setQuakeColour(TextView txtMagnitude, float mag){
+        if (mag < -0.7){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.light_steel_blue));
+        }
+        else if (mag >= -0.7 && mag < -0.3){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.powder_blue));
+        }
+        else if (mag >= -0.3 && mag < 0.2){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.aqua));
+        }
+        else if (mag >= 0.2 && mag < 0.5){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.light_sea_green));
+        }
+        else if (mag >= 0.5 && mag < 0.8){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.medium_sea_green));
+        }
+        else if (mag >= 0.8 && mag < 1.1){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.khaki));
+        }
+        else if (mag >= 1.1 && mag < 1.4){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.yellow));
+        }
+        else if (mag >= 1.4 && mag < 1.8){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.dark_orange));
+        }
+        else if (mag >= 1.8 && mag < 2.2){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.orange_red));
+        }
+        else if (mag >= 2.2 && mag < 2.5){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.red));
+        }
+        else if (mag >= 2.5){
+            txtMagnitude.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.dark_red));
+        }
     }
 }
